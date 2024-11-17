d2a52d868b977dd7258fac5adf43811acd8c84bc
hatim <hatim1300@gmail.com>
2024-11-17 07:04:53 +0400

    update

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://zenweiswpivszgkjxlcl.supabase.co";
+export const supabaseUrl = "https://jfuanasffvjeqsyvrnki.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplbndlaXN3cGl2c3pna2p4bGNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA3NzcxNTQsImV4cCI6MjAxNjM1MzE1NH0.-9u3mc0ISUC-KvPsVx0yA1q1agTjL0XAobJ8VMRqjyE";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpmdWFuYXNmZnZqZXFzeXZybmtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE3NDMxOTMsImV4cCI6MjA0NzMxOTE5M30.pJUouNxzbNPFx0OskxnW2t4ufZP2DhHSoo7CMqgHqvw";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
